2a0ce839853d4cf934803b4e039c3607d90c82b0
fmcubium <andresdelaflorad@gmail.com>
2025-10-05 15:48:10 -0400

    Fixed migrations not working!

--- backend/quickstart/views.py
+++ backend/quickstart/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from .models import Users
+from .models import StudyUser
 
 # Create your views here.
 
 @api_view(['GET',])
 def ping(request):
     if request.method == 'GET':
-        print(type(User))
+        print(type(StudyUser))
         return Response(b"pong")
