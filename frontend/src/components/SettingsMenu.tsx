6bd7b52fb80a50aab23972107255b0318bd03b79
Michael Capelli <michael.capelli@ufl.edu>
2025-10-15 18:32:00 -0400

    added dependecies for shadcn

---
+++ frontend/src/components/SettingsMenu.tsx
@@ -0,0 +1,31 @@
+import "./SettingsMenu.css";
+
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function SettingsMenu() {
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+          <CardAction>Card Action</CardAction>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
