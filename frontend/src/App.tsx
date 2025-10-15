616447856f60bdde53f55d58c368148195c1768b
Michael Capelli <michael.capelli@ufl.edu>
2025-10-15 18:32:00 -0400

    added dependecies for shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import oscLogo from "./assets/osc-logo.png"
-import githubLogo from "./assets/github-mark-white.svg"
-import './App.css'
-import Timer from './Timer'
-import ToDoList from './ToDoList'
+import { useState } from "react";
+import oscLogo from "./assets/osc-logo.png";
+import githubLogo from "./assets/github-mark-white.svg";
+import "./App.css";
+import Timer from "./Timer";
+import ToDoList from "./ToDoList";
+import SettingsMenu from "./components/SettingsMenu";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -23,20 +24,17 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Welcome to the Studygatchi Project!
-        </p>
+        <p>Welcome to the Studygatchi Project!</p>
       </div>
       <Timer />
       <ToDoList />
-      <p className="read-the-docs">
-        Click on the OSC link to get to OSC!
-      </p>
+      <SettingsMenu />
+      <p className="read-the-docs">Click on the OSC link to get to OSC!</p>
       <p className="read-the-docs">
         Click on the Github Link to get to this project's GitHub repository!
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
