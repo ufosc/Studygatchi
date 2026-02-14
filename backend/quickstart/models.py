ab3cbef03e1890a66e3150b1d01ef1b5ee0de062
Kamron Arabi <kamronarabi@gmail.com>
2026-02-14 13:46:14 -0500

    Goober model made

--- backend/quickstart/models.py
+++ backend/quickstart/models.py
@@ -15,3 +15,11 @@ class Task(models.Model):
     due_date = models.DateTimeField()
     description = models.TextField()
     username = models.ForeignKey(StudyUser, on_delete=models.CASCADE)
+
+class Goober(models.Model):
+    # id will be automatically created by Django as PRIMARY KEY
+    name = models.CharField(max_length=255)
+    health = models.IntegerField(default=100)
+    image_url = models.URLField()
+    equipped_clothing = models.CharField(max_length=255, null=True, blank=True)
+    owner_user = models.ForeignKey(StudyUser, on_delete=models.CASCADE)
