eecd3bc52b8757186c0e63c4d158879460dc0a2d
Andres D <84483578+fmcubium@users.noreply.github.com>
2026-06-14 14:16:39 -0400

    Added Pet to models.py on main

--- backend/quickstart/models.py
+++ backend/quickstart/models.py
@@ -7,6 +7,18 @@ class StudyUser(AbstractUser):
     # inherits: username, password, email, ...
     money = models.IntegerField(default=100)
 
+
+class Pet(models.Model):
+    # ID automatically created by Django
+    type = models.TextField(default="pet")
+    xp = models.IntegerField(default=0)
+    level = models.IntegerField(default=1)
+    happiness = models.DecimalField(default=0.5, decimal_places=2, max_digits=3)
+    equipped_clothes = models.IntegerField(null=True)
+    image_url = models.URLField(null=True) # Change in the future
+    owner = models.ForeignKey(StudyUser, on_delete=models.CASCADE)
+
+
 class Task(models.Model):
     # task_id will be automatically created by Django
     reward = models.IntegerField(default=0)
